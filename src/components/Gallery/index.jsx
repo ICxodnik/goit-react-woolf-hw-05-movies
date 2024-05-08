41c18abd1b56151d194bef6a7f477e2234717156
Natalia <icxodnik@gmail.com>
2024-05-08 16:34:32 +0200

    move info

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -3,14 +3,6 @@ import css from 'components/Gallery/index.module.css';
 import { GalleryItem } from './GalleryItem';
 
 export const Gallery = props => {
-  if (!props.films.length) {
-    return (
-      <div className={css.info}>
-        There are no movies that matched your query
-      </div>
-    );
-  }
-
   return (
     <div className={css.gallery}>
       {props.films.map(el => (
