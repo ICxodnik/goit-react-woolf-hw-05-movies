3f7a8d9c7d33a97b79c9534815d4c8a95b84f6bd
Natalia <icxodnik@gmail.com>
2024-05-08 16:31:07 +0200

    add styles to main page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Reviews } from 'components/Reviews';
 
 export const App = () => {
   return (
-    <div>
+    <div className="mainPage">
       <nav>
         <NavLink to="/" end>
           Home
